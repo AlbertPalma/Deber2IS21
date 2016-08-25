2dfc2a44bcd61aab54b86ee7a464f0aa0385e247
Alberto <arnalpal95@gmail.com>
2016-08-25 16:23:51 -0500

    Eliminar lineas de codigo

--- Deber2IS2/src/deber2is2/Clase1.java
+++ Deber2IS2/src/deber2is2/Clase1.java
@@ -1,21 +1,12 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package deber2is2;
 
-/**
- *
- * @author arnal
- */
+
 public class Clase1 {
     
     private int nume1, num2;
     
     public void suma(){
-        if(nume1==0){
-            System.out.println("No se puede mostrar le falta el valor 2");
-        }
+        
     }
 }
