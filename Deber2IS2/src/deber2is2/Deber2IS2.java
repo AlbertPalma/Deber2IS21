0a793853153c29f1ce5cb8a4afcb087fd79eb40d
Alberto <arnalpal95@gmail.com>
2016-08-25 16:22:55 -0500

    Creacion de una caculadora simple

--- Deber2IS2/src/deber2is2/Deber2IS2.java
+++ Deber2IS2/src/deber2is2/Deber2IS2.java
@@ -5,6 +5,8 @@
  */
 package deber2is2;
 
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author arnal
@@ -15,8 +17,22 @@ public class Deber2IS2 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
-        System.out.println("Hola Mundo e Ing XD");
+       String strNumero1;
+      String strNumero2;
+      int intNumero1;   
+      int intNumero2;   
+      int suma;
+      strNumero1 = JOptionPane.showInputDialog( "Ingrese el entero 1" );
+      strNumero2 = JOptionPane.showInputDialog( "Ingrese el entero 2" );
+      intNumero1 = Integer.parseInt( strNumero1 );
+      intNumero2 = Integer.parseInt( strNumero2 );
+
+      suma = intNumero1 + intNumero2;
+
+      JOptionPane.showMessageDialog(
+         null, "La suma es " + suma, "Resultado,",
+         JOptionPane.PLAIN_MESSAGE );
+     System.exit( 0 );  
     }
     
 }
