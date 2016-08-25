eabd959715b80b10e71ad0dbb4122de3a5b442c3
Alberto <arnalpal95@gmail.com>
2016-08-25 15:57:15 -0500

    Correccion del Mensaje

---
+++ Deber2IS2/src/deber2is2/Clase1.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package deber2is2;
+
+/**
+ *
+ * @author arnal
+ */
+public class Clase1 {
+    
+    private int nume1, num2;
+    
+    public void suma(){
+        System.out.println("Aqui se va a realiza una suma ");
+    }
+}
